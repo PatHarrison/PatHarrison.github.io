ca5b1befa31c15b4f8230b3e322c32bb7e9b7894
PatHarrison <patrickgharrison@outlook.com>
2025-07-28 23:33:33 -0600

    pelican quickstart

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+AUTHOR = 'Patrick Harrison'
+SITENAME = 'Patrick Harrison'
+SITEURL = "https://patharrison.github.io"
+
+PATH = "content"
+
+TIMEZONE = 'UTC'
+
+DEFAULT_LANG = 'English'
+
+THEME = "../pelican-mistwood" # theme path for dev
+#THEME = "theme/pelican-mistwood" # theme path for prod
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+)
+
+# Social widget
+SOCIAL = (
+        ("LinkedIn", "https://linkedin.com/in/patrickgharrison"),
+        ("Github", "https://github.com/PatHarrison"),
+)
+
+DEFAULT_PAGINATION = 3
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
