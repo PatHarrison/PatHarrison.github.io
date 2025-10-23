d01a50278fb1c0841db0d22db0aa612ba323b88e
PatHarrison <patrickgharrison@outlook.com>
2025-10-23 14:40:34 -0600

    personalized config files

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,28 +8,15 @@ TIMEZONE = 'America/Edmonton'
 
 DEFAULT_LANG = 'en'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
 # Blogroll
 LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
 )
 
 # Social widget
 SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
+    ("Mail", "mailto:patrickgharrison@outlook.com"),
+    ("LinkedIn", "https://linkedin.com/in/patrickgharrison"),
+    ("Github", "https://github.com/PatHarrison"),
 )
 
 DEFAULT_PAGINATION = 5
-
-# Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
