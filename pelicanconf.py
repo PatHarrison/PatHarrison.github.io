1ec52152d45a90d94dee0624942e950da4960f5c
PatHarrison <patrickgharrison@outlook.com>
2025-10-23 15:11:39 -0600

    added link content in pelicanconf

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,8 @@
 AUTHOR = 'Patrick Harrison'
 SITENAME = 'Patrick Harrison | Portfolio'
 TAGLINE = "GIS | Software Automation | Astrophysics"
+SOURCE_LINK = "https://github.com/PatHarrison/patharrison.github.io"
+
 SITEURL = ""
 
 PATH = "content"
